ab0708c26a5ca18b015169b5ecb04198d33e9706
Administrator <406967240@qq.com>
2019-11-17 10:58:56 +0800

    简单优化后的随机密码生成器

--- src/main/java/com/swqsv/main/RandomPassword.java
+++ src/main/java/com/swqsv/main/RandomPassword.java
@@ -1,32 +1,31 @@
 package com.swqsv.main;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Lin_YongLiang
  */
 public class RandomPassword {
-    /**
-     * 密码的长度
-     */
-    private static int passwordLength;
+
     /**
      * 生成的密码
      */
-    private static StringBuffer password = new StringBuffer();
+    private static StringBuilder password = new StringBuilder();
 
     public static void main(String[] args) {
-        //初始化数字英文和符号
+        //密码的长度
+        int passwordLength;
+        //初始化数字英文和符号,可以根据需要添加
         String num = "0123456789";
         String english = "qwertyuiopasdfghjklzxcvbnm";
         String englishBig = "QWERTYUIOPASDFGHJKLZXCVBNM";
         String symbol = "!@#$%^&*_+-{}<>.*";
         String stringSum = num + english + englishBig + symbol;
         int length = stringSum.length();
-        //定义密码长度,写死8
+        //定义密码长度,写死8,可以根据需要填写
         passwordLength = 8;
         for (int i = 0; i < passwordLength; i++) {
             Random random = new Random();
